d151903721c75a4229fa922d7cdf3ddbf8c1ba50
IsabellaFrancesconi <irf9@case.edu>
2025-03-27 17:51:48 -0400

    Scaffolding api

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # Main Flask App placeholders 
 from flask import Flask, request, jsonify
-from match import calculate_match
 
 app = Flask(__name__)
 
@@ -8,13 +7,38 @@ app = Flask(__name__)
 def home():
     return "RoomieMatch API is running"
 
-@app.route('/get_matches', methods=['POST'])
-def get_matches():
-    user_data = request.json.get("user_data")
-    all_users = request.json.get("all_users")
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    user_data = request.json
 
-    matches = calculate_match(user_data, all_users)
+    print("CREATING USER", user_data)
+
+    user_id = 420
+
+    return jsonify({ "user": user_id })
+
+@app.route('/get_matches')
+def get_matches():
+    user_id = request.args.get("user")
+
+    # TODO compute matches with user``
+    matches = [
+        { "user_id": 320, "score": 0.5 },
+        { "user_id": 20, "score": 0.7 },
+    ]
+
     return jsonify(matches)
 
+@app.route('/user')
+def user():
+    user_id = request.args.get("user")
+
+    # TODO retrieve user data from db
+    user_data = { "field1": 0, "field2": 1 }
+
+    return jsonify(user_data)
+
 if __name__ == '__main__':
+
+
     app.run(debug=True)
